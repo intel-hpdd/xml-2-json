1762e60fba383f722db600e2345bc72a6c95d313
Will Johnson <wjohnson@whamcloud.com>
2018-07-18 17:08:07 -0400

    Update copyright message
    Signed-off-by: Will Johnson <wjohnson@whamcloud.com>

--- source/xml-2-json.js
+++ source/xml-2-json.js
@@ -1,6 +1,9 @@
 // @flow
 
-// Copyright (c) 2018 DDN
+//
+// Copyright (c) 2018 DDN. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
 
 import libXml from 'node-xml';
 
